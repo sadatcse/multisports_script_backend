ea2d8f28821c8d0dd26207ae29d09cc1b6aac8d5
sadatcse <sadatcse@gmail.com>
2025-07-28 18:26:47 +0600

    finished

--- server.js
+++ server.js
@@ -81,8 +81,8 @@ app.use("/api", routes);
 app.use(errorHandler);
 
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Server is running." });
 });
 
 // Error handling middleware
